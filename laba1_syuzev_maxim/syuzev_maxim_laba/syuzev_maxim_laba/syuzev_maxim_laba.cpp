d7a43da12ac5173698302688480bd285652d4a91
Maxim050903 <cuzevm@mail.ru>
2022-09-21 16:26:50 +0300

    меню
    создано меню

--- laba1_syuzev_maxim/syuzev_maxim_laba/syuzev_maxim_laba/syuzev_maxim_laba.cpp
+++ laba1_syuzev_maxim/syuzev_maxim_laba/syuzev_maxim_laba/syuzev_maxim_laba.cpp
@@ -2,7 +2,18 @@
 #include <string>
 using namespace std;
 
-int main() 
+int main()
 {
-	
+	while (true)
+	{
+		cout << "1.add pipe\n"
+			<< "2.add cs\n"
+			<< "3.view all objects\n"
+			<< "3.edit pipe\n"
+			<< "4.edit cs\n"
+			<< "5.save\n"
+			<< "6.download\n"
+			<< "7.exit\n";
+		return(0);
+	}
 }
