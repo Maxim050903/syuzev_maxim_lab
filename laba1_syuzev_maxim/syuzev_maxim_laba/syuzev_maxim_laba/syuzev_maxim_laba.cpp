b37cd9de966eae807577b3647e743ff0652e2873
Maxim050903 <cuzevm@mail.ru>
2022-09-21 17:11:08 +0300

    switch and case
    добавлены использование switch и case

--- laba1_syuzev_maxim/syuzev_maxim_laba/syuzev_maxim_laba/syuzev_maxim_laba.cpp
+++ laba1_syuzev_maxim/syuzev_maxim_laba/syuzev_maxim_laba/syuzev_maxim_laba.cpp
@@ -2,18 +2,39 @@
 #include <string>
 using namespace std;
 
+struct pipe
+{
+	float length;
+	double diameter;
+	bool condition;
+
+};
+
 int main()
 {
-	while (true)
-	{
 		cout << "1.add pipe\n"
-			<< "2.add cs\n"
-			<< "3.view all objects\n"
-			<< "3.edit pipe\n"
-			<< "4.edit cs\n"
-			<< "5.save\n"
-			<< "6.download\n"
-			<< "7.exit\n";
-		return(0);
-	}
+			 << "2.add cs\n"
+			 << "3.view all objects\n"
+			 << "3.edit pipe\n"
+			 << "4.edit cs\n"
+			 << "5.save\n"
+			 << "6.download\n"
+			 << "7.exit\n";
+		int id;
+		cin >> id;
+		switch (id)
+		{
+			case 1:
+			{
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+			case 3:
+			{
+				cout << "durik";
+			}
+		}
 }
